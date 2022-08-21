a34a2a9eaf17d1ce00245e0cd954c2492c1a717f
Sokoloff-M <109232419+Sokoloff-M@users.noreply.github.com>
2022-08-21 20:43:18 +0800

    Add files via upload

---
+++ class_figure.py
@@ -0,0 +1,16 @@
+class Rectangle:
+    def __init__(self, height: int, widght: int, sqare: int):
+        self.widght = widght
+        self.height = height
+        self.sqare = sqare
+
+
+r1 = Rectangle()
+
+r1.height = 15
+r1.widght = 43
+r1.sqare = 645
+
+
+class Circle:
+    pass
