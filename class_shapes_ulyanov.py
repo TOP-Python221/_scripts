6f77af405277da06986243e333c28e2424293b36
UlyanovSergey <108969866+UlyanovSergey@users.noreply.github.com>
2022-08-21 17:58:30 +0300

    добавил файл class_shapes_ulyanov

--- class_shapes_ulyanov.py
+++ class_shapes_ulyanov.py
@@ -1,3 +1,4 @@
+PI = 3.14
 class Rectangle:
     def __init__(self, height: int, width: int):
         self.height = height
@@ -7,10 +8,12 @@ class Rectangle:
 
 
 class Circle:
-    def __init__(self, radius: int, circlength: int, area: int):
-        self.rabius = radius
-        self.circlenght = circlength
-        self.area = area
+    def __init__(self, radius: int):
+        self.radius = radius
+        self.area = PI * self.radius ** 2
+        self.circlelengs = 2 * PI * self.radius ** 2
 
+c1 = Circle(12)
+print(f'площадь круга = {c1.area}, длина окружности = {c1.circlelengs}')
 r1 = Rectangle(20, 40)
 print(r1.area)
