668ffee12defa1960449b702cc2f9bae41114d23
Gennadiy <renardes@gmail.com>
2022-08-27 15:33:37 +0500

    скрытые и защищённые атрибуты

---
+++ class_attr1.py
@@ -0,0 +1,29 @@
+class Student:
+    def __init__(self,
+                 student_id: str,
+                 group: str,
+                 year: int,
+                 grade_ave: float):
+        self.__id = student_id
+        self.group = group
+        self.year = year
+        self._grade_ave = grade_ave
+
+    def load_from_db(self):
+        # ... self.__id ...
+        pass
+
+    def update_in_db(self):
+        # ... self.__id ...
+        pass
+
+
+st1 = Student(
+    '34l5sd09vwo34tr5w',
+    'PT-301',
+    4,
+    4.4
+)
+print(f'\nпространство имён:\n{st1.__dict__ = }')
+scope = [name for name in dir(st1) if not name.startswith('__')]
+print(f'\nобласть видимости:\n{scope = }\n')
