cf5b8ee4af02bad4fa3780bbf4197401208a76a0
Gennadiy <renardes@gmail.com>
2022-10-08 16:00:40 +0500

    шаблон Мост — более тесное взаимодействие сущностей, соединённых мостами

--- patterns/structural/bridge/bridge2.py
+++ patterns/structural/bridge/bridge2.py
@@ -2,30 +2,39 @@ from abc import ABC, abstractmethod
 
 
 class Material(ABC):
+    hits: float
     @abstractmethod
     def __str__(self):
         pass
 
 class Straw(Material):
+    hits = 0.75
     def __str__(self):
         return 'straw'
 
 class Wood(Material):
+    hits = 1
     def __str__(self):
         return 'wooden'
 
 class Cobblestone(Material):
+    hits = 1.5
     def __str__(self):
         return 'cobblestone'
 
 class Brick(Material):
+    hits = 1.25
     def __str__(self):
         return 'brick'
 
 
+
 class Building(ABC):
+    base_hitpoints: int
+
     def __init__(self, material: Material):
         self.material = material
+        self.hitpoints = int(self.base_hitpoints * material.hits)
 
     @abstractmethod
     def __str__(self):
@@ -33,21 +42,25 @@ class Building(ABC):
 
 
 class Watchtower(Building):
+    base_hitpoints = 300
+
     def __init__(self, name: str, material: Material):
         super().__init__(material)
         self.name = name
 
     def __str__(self):
-        return f'{self.material} watchtower {self.name}'
+        return f'{self.material} watchtower {self.name}: {self.hitpoints} HP'
 
 
 class Mill(Building):
+    base_hitpoints = 150
+
     def __init__(self, name: str, material: Material):
         super().__init__(material)
         self.name = name
 
     def __str__(self):
-        return f'{self.material} mill {self.name}'
+        return f'{self.material} mill {self.name}: {self.hitpoints} HP'
 
 
 wt1 = Watchtower('The Long Shadow Tower of West Edge', Wood())
