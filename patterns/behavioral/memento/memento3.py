571407d421c2e066d8fc0ac14a47460cf3c72cb4
Gennadiy <renardes@gmail.com>
2022-10-22 17:53:00 +0500

    шаблон Хранитель — тест полной инфраструктуры

--- patterns/behavioral/memento/memento3.py
+++ patterns/behavioral/memento/memento3.py
@@ -36,6 +36,15 @@ class Character:
         self.dead: bool = False
         self.position: dict[str, int] = {'x': 0, 'y': 0}
 
+    def __repr__(self):
+        result = (f"<{self.name}:"
+                  f" POS=({self.position['x']};{self.position['y']}),"
+                  f" LVL={self.level},"
+                  f" HP={self.health}>")
+        for item in self.inventory:
+            result += f'\n\t{item}'
+        return result
+
     def __str__(self):
         return self.name
 
@@ -48,28 +57,32 @@ class Character:
             self.health -= 1
         else:
             self.dead = True
+            print('\nИГРА ОКОНЧЕНА\n')
 
     def level_up(self) -> None:
         self.level += 1
 
     def pick_item(self, item: str) -> None:
-        self.inventory += [item]
+        self.inventory += [item.lower()]
 
     def drop_item(self, item: str) -> None:
+        item = item.lower()
         if item in self.inventory:
             self.inventory.remove(item)
 
     @property
     def state(self) -> CharacterState:
+        """"""
         return CharacterState(
             self.level,
             self.health,
-            self.position,
-            self.inventory
+            self.position.copy(),
+            self.inventory.copy()
         )
 
     @state.setter
     def state(self, save: CharacterState) -> None:
+        """"""
         self.level = save.level
         self.health = save.health
         self.position = save.position
@@ -115,3 +128,46 @@ class SaveLoadMenu:
         self.character.state = self.__saves[i]
 
 
+
+hero = Character('Дюк')
+print(repr(hero))
+
+menu = SaveLoadMenu(hero)
+
+hero.move(1, 0)
+hero.pick_item('пистолет')
+hero.move(2, 0)
+hero.hit()
+hero.hit()
+hero.level_up()
+hero.move(-2, 1)
+print(repr(hero))
+
+menu.save()
+
+hero.move(0, 2)
+hero.pick_item('нож')
+hero.move()
+hero.hit()
+hero.hit()
+hero.hit()
+hero.drop_item('пистолет')
+hero.move(3, -4)
+print(repr(hero))
+
+menu.save()
+
+hero.move()
+hero.pick_item('патроны к винтовке')
+hero.move(0, 1)
+hero.hit()
+hero.hit()
+hero.hit()
+hero.hit()
+hero.hit()
+hero.hit()
+print(repr(hero))
+
+menu.load()
+
+print(repr(hero))
