2918e7b846af6009d047bc1cc572963b84d9e388
Gennadiy <renardes@gmail.com>
2022-11-12 16:15:00 +0500

    MVC 1: CLI представление

---
+++ mvc/1/view.py
@@ -0,0 +1,31 @@
+"""View (MVC): CLI представление."""
+
+from time import sleep
+
+
+def start_view():
+    """Отображает приветствие."""
+    print('\nДобро пожаловать в демонстратор MVC!')
+    sleep(2)
+
+
+def ask_view():
+    """Отображает вопрос."""
+    print('\nВывести все записи из БД? [y/n] > ', end='')
+
+
+def answer_view() -> str:
+    """Получает и возвращает ответ."""
+    return input()
+
+
+def show_all(people: tuple):
+    """Отображает всех людей построчно."""
+    print(*people, sep='\n')
+    sleep(10)
+
+
+def end_view():
+    """Отображает прощание."""
+    print('\nПока!')
+    sleep(2)
