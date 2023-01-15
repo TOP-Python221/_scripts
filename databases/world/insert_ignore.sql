98d250bb88664ff9aaa9d2089788ad94f6894177
Gennadiy <renardes@gmail.com>
2023-01-15 16:13:28 +0500

    добавление записей в таблицу

---
+++ databases/world/insert_ignore.sql
@@ -0,0 +1,8 @@
+insert ignore into `city`
+	(`ID`, `CountryCode`, `District`, `Name`, `Population`)
+values
+    (1, 'RUS', 'Sverdlovsk', 'Lesnoi', 49329),
+	(2, 'RUS', 'Dagestan', 'Kizljar', 49174),
+	(3, 'RUS', 'Kemerovo', 'Osinniki', 40790),
+	(4, 'RUS', 'Sverdlovsk', 'Verhnjaja Salda', 40520),
+    (4087, 'RUS', 'Sverdlovsk', 'Rez', 36260);
---
+++ databases/world/insert_values.sql
@@ -0,0 +1,7 @@
+insert into `city`
+	(`CountryCode`, `District`, `Name`, `Population`)
+values
+	('RUS', 'Sverdlovsk', 'Lesnoi', 49329),
+	('RUS', 'Dagestan', 'Kizljar', 49174),
+	('RUS', 'Kemerovo', 'Osinniki', 40790),
+	('RUS', 'Sverdlovsk', 'Verhnjaja Salda', 40520);
