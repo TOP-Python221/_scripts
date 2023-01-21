dc58586b35c1db3e8d29bb02ed3966e278fd087f
Gennadiy <renardes@gmail.com>
2023-01-21 18:15:52 +0500

    проверка работы ограничений

---
+++ databases/mus_collection/work_constraint_check.sql
@@ -0,0 +1,9 @@
+insert into `artists`
+	(`name`)
+values
+	('Artist 1'),
+    ('Artist 2'),
+    (''),
+    ('Artist 3');
+
+select * from `artists`;
---
+++ databases/mus_collection/work_constraint_primary_key.sql
@@ -0,0 +1,14 @@
+insert into `countries`
+	(`id`, `name`)
+values
+	(null, 'abc');
+
+select * from `countries`;
+
+insert into `countries`
+	(`id`, `name`)
+values
+	(null, 'def'),
+    (null, 'xyz');
+    
+select * from `countries`;
