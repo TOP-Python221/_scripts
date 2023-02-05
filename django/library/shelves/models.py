2fb87f1515e6a851a406d5966e4aa57d99d1f052
Gennadiy <renardes@gmail.com>
2023-02-05 13:31:26 +0500

    создание экземпляров модели, сохранение их в базу данных

--- django/library/shelves/models.py
+++ django/library/shelves/models.py
@@ -5,14 +5,23 @@ class Author(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title}'
 
 
 class Publisher(models.Model):
     name = models.CharField(max_length=50)
     authors = models.ManyToManyField(Author)
     books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return f'{self.name}'
 
